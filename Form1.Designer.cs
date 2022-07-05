67885c55c7f5af8e4801f2b6d996dc8501c3811b
electrosan <sanket.khamkar@electrosan.co.in>
2022-07-05 17:48:13 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aPIFunctionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clloginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clgetmodelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cllockokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clgetidToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clgetbatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clsetsignToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clgetsignToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clsetosignToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clgetosignToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clwriteblockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clreadblockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clwriteblock2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clreadblock2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clwritewordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clreadwordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cllogoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aPIFunctionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(673, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aPIFunctionsToolStripMenuItem
+            // 
+            this.aPIFunctionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clloginToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.clgetmodelToolStripMenuItem,
+            this.cllockokToolStripMenuItem,
+            this.clgetidToolStripMenuItem,
+            this.clgetbatchToolStripMenuItem,
+            this.clsetsignToolStripMenuItem,
+            this.clgetsignToolStripMenuItem,
+            this.clsetosignToolStripMenuItem,
+            this.clgetosignToolStripMenuItem,
+            this.clwriteblockToolStripMenuItem,
+            this.clreadblockToolStripMenuItem,
+            this.clwriteblock2ToolStripMenuItem,
+            this.clreadblock2ToolStripMenuItem,
+            this.clwritewordToolStripMenuItem,
+            this.clreadwordToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.cllogoutToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.aPIFunctionsToolStripMenuItem.Name = "aPIFunctionsToolStripMenuItem";
+            this.aPIFunctionsToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.aPIFunctionsToolStripMenuItem.Text = "API Functions";
+            // 
+            // clloginToolStripMenuItem
+            // 
+            this.clloginToolStripMenuItem.Name = "clloginToolStripMenuItem";
+            this.clloginToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clloginToolStripMenuItem.Text = "1.  cl_login";
+            this.clloginToolStripMenuItem.Click += new System.EventHandler(this.clloginToolStripMenuItem_Click);
+            // 
+            // clgetmodelToolStripMenuItem
+            // 
+            this.clgetmodelToolStripMenuItem.Name = "clgetmodelToolStripMenuItem";
+            this.clgetmodelToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clgetmodelToolStripMenuItem.Text = "2.  cl_get_model";
+            this.clgetmodelToolStripMenuItem.Click += new System.EventHandler(this.clgetmodelToolStripMenuItem_Click);
+            // 
+            // cllockokToolStripMenuItem
+            // 
+            this.cllockokToolStripMenuItem.Name = "cllockokToolStripMenuItem";
+            this.cllockokToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.cllockokToolStripMenuItem.Text = "3.  cl_lock_ok";
+            this.cllockokToolStripMenuItem.Click += new System.EventHandler(this.cllockokToolStripMenuItem_Click);
+            // 
+            // clgetidToolStripMenuItem
+            // 
+            this.clgetidToolStripMenuItem.Name = "clgetidToolStripMenuItem";
+            this.clgetidToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clgetidToolStripMenuItem.Text = "4.  cl_get_id";
+            this.clgetidToolStripMenuItem.Click += new System.EventHandler(this.clgetidToolStripMenuItem_Click);
+            // 
+            // clgetbatchToolStripMenuItem
+            // 
+            this.clgetbatchToolStripMenuItem.Name = "clgetbatchToolStripMenuItem";
+            this.clgetbatchToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clgetbatchToolStripMenuItem.Text = "5.  cl_get_batch";
+            this.clgetbatchToolStripMenuItem.Click += new System.EventHandler(this.clgetbatchToolStripMenuItem_Click);
+            // 
+            // clsetsignToolStripMenuItem
+            // 
+            this.clsetsignToolStripMenuItem.Name = "clsetsignToolStripMenuItem";
+            this.clsetsignToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clsetsignToolStripMenuItem.Text = "6.  cl_set_sign";
+            this.clsetsignToolStripMenuItem.Click += new System.EventHandler(this.clsetsignToolStripMenuItem_Click);
+            // 
+            // clgetsignToolStripMenuItem
+            // 
+            this.clgetsignToolStripMenuItem.Name = "clgetsignToolStripMenuItem";
+            this.clgetsignToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clgetsignToolStripMenuItem.Text = "7.  cl_get_sign";
+            this.clgetsignToolStripMenuItem.Click += new System.EventHandler(this.clgetsignToolStripMenuItem_Click);
+            // 
+            // clsetosignToolStripMenuItem
+            // 
+            this.clsetosignToolStripMenuItem.Name = "clsetosignToolStripMenuItem";
+            this.clsetosignToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clsetosignToolStripMenuItem.Text = "8. cl_set_osign";
+            this.clsetosignToolStripMenuItem.Click += new System.EventHandler(this.clsetosignToolStripMenuItem_Click);
+            // 
+            // clgetosignToolStripMenuItem
+            // 
+            this.clgetosignToolStripMenuItem.Name = "clgetosignToolStripMenuItem";
+            this.clgetosignToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clgetosignToolStripMenuItem.Text = "9.  cl_get_osign";
+            this.clgetosignToolStripMenuItem.Click += new System.EventHandler(this.clgetosignToolStripMenuItem_Click);
+            // 
+            // clwriteblockToolStripMenuItem
+            // 
+            this.clwriteblockToolStripMenuItem.Name = "clwriteblockToolStripMenuItem";
+            this.clwriteblockToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clwriteblockToolStripMenuItem.Text = "10. cl_write_block (1)";
+            this.clwriteblockToolStripMenuItem.Click += new System.EventHandler(this.clwriteblockToolStripMenuItem_Click);
+            // 
+            // clreadblockToolStripMenuItem
+            // 
+            this.clreadblockToolStripMenuItem.Name = "clreadblockToolStripMenuItem";
+            this.clreadblockToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clreadblockToolStripMenuItem.Text = "11. cl_read_block (1)";
+            this.clreadblockToolStripMenuItem.Click += new System.EventHandler(this.clreadblockToolStripMenuItem_Click);
+            // 
+            // clwriteblock2ToolStripMenuItem
+            // 
+            this.clwriteblock2ToolStripMenuItem.Name = "clwriteblock2ToolStripMenuItem";
+            this.clwriteblock2ToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clwriteblock2ToolStripMenuItem.Text = "12. cl_write_block (2)";
+            this.clwriteblock2ToolStripMenuItem.Click += new System.EventHandler(this.clwriteblock2ToolStripMenuItem_Click);
+            // 
+            // clreadblock2ToolStripMenuItem
+            // 
+            this.clreadblock2ToolStripMenuItem.Name = "clreadblock2ToolStripMenuItem";
+            this.clreadblock2ToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clreadblock2ToolStripMenuItem.Text = "13. cl_read_block (2)";
+            this.clreadblock2ToolStripMenuItem.Click += new System.EventHandler(this.clreadblock2ToolStripMenuItem_Click);
+            // 
+            // clwritewordToolStripMenuItem
+            // 
+            this.clwritewordToolStripMenuItem.Name = "clwritewordToolStripMenuItem";
+            this.clwritewordToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clwritewordToolStripMenuItem.Text = "14. cl_write_word";
+            this.clwritewordToolStripMenuItem.Click += new System.EventHandler(this.clwritewordToolStripMenuItem_Click);
+            // 
+            // clreadwordToolStripMenuItem
+            // 
+            this.clreadwordToolStripMenuItem.Name = "clreadwordToolStripMenuItem";
+            this.clreadwordToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.clreadwordToolStripMenuItem.Text = "15. cl_read_word";
+            this.clreadwordToolStripMenuItem.Click += new System.EventHandler(this.clreadwordToolStripMenuItem_Click);
+            // 
+            // cllogoutToolStripMenuItem
+            // 
+            this.cllogoutToolStripMenuItem.Name = "cllogoutToolStripMenuItem";
+            this.cllogoutToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.cllogoutToolStripMenuItem.Text = "16. cl_logout";
+            this.cllogoutToolStripMenuItem.Click += new System.EventHandler(this.cllogoutToolStripMenuItem_Click_1);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 308);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(673, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(180, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(180, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 330);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aPIFunctionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clloginToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem clgetmodelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cllockokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clgetidToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clgetbatchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clsetsignToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clgetsignToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clsetosignToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clgetosignToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clwriteblockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clreadblockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clwriteblock2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clreadblock2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clwritewordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clreadwordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cllogoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Specify the COPYLOCK DLL to be used based on application mode (Single User or Multi-user)
+        // you may also rename original default DLL file name and use this name here.
+        const String dll_COPYLOCK = "xCL32.DLL"; //Standalone application
+
+//        const String dll_COPYLOCK = "NETCL32.DLL"; //Multiuser application
+
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_login(string name);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_get_model([MarshalAs(UnmanagedType.LPArray)] byte[] dModel);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_lock_ok();
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_get_id([MarshalAs(UnmanagedType.LPArray)] byte[] dID, String rPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_get_batch([MarshalAs(UnmanagedType.LPArray)] byte[] dBatch, String rPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_set_sign(String dSign, String wPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_get_sign([MarshalAs(UnmanagedType.LPArray)] byte[] dSign, String rPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_set_osign(String dSign, String wPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_get_osign([MarshalAs(UnmanagedType.LPArray)] byte[] dSign, String rPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_write_block(String dBuf, String wPass, int BlockNo, int StPos, int Count);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_read_block([MarshalAs(UnmanagedType.LPArray)] byte[] dBuf, String rPass, int BlockNo, int StPos, int Count);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_write_word(int dWord, String wPass, int BlockNo, int StPos);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_read_word(ref int dWord, String rPass, int BlockNo, int StPos);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_get_count(ref int dCo, String rPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_dec_count(int dVal, String wPass);
+        [DllImport(dll_COPYLOCK)]
+        public static extern int cl_logout();
+
+        private void DisplayResult(int dResult, string data)
+        {
+            switch (dResult)
+            {
+                case 1:
+                    if (data == "")
+                    {
+                        toolStripStatusLabel1.Text = "Success" ;
+                    }
+                    else
+                    {
+                        toolStripStatusLabel1.Text = "Success" + ";   Dongle Data = " + data;
+                    }
+                    break;
+
+                case -1:
+                    toolStripStatusLabel1.Text = "Error#-1: Lock is missing" ;
+                    break;
+
+                case -2:
+                    toolStripStatusLabel1.Text = "Error#-2: Driver load Error" ;
+                    break;
+
+                case -3:
+                    toolStripStatusLabel1.Text = "Error#-3: Incorrect password" ;
+                    break;
+
+                case -4:
+                    toolStripStatusLabel1.Text = "Error#-4: Fascility N.A." ;
+                    break;
+
+                case -5:
+                    toolStripStatusLabel1.Text = "Error#-5: Write failed" ;
+                    break;
+
+                case -6:
+                    toolStripStatusLabel1.Text = "Error#-6: Login users limit is over" ;
+                    break;
+
+                case -7:
+                    toolStripStatusLabel1.Text = "Error#-7: Link to the server is not active" ;
+                    break;
+
+                case -9:
+                    toolStripStatusLabel1.Text = "Error#-9: Cannot establish connection with server" ;
+                    break;
+
+                case -12 :
+                   toolStripStatusLabel1.Text = "Error#-12: Time period expired";
+                   break;
+
+                default:
+
+                    toolStripStatusLabel1.Text = "Error#nn: Internal Error";
+                    break;
+            }
+        }
+
+        private void clloginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int dResult = cl_login("032140501240040622010232");
+            int myCo = 0;
+            String rPass = "";   //read authentication password
+
+            DisplayResult(dResult, "") ;
+            if (dResult == 1)
+            {
+                dResult = cl_get_count(ref myCo, rPass);
+                dResult = cl_dec_count(1, rPass);
+                dResult = cl_get_count(ref myCo, rPass);
+            }
+        }
+
+        private void clgetmodelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dModel = new byte[100]; 
+            String DongleModel = "";
+            int dResult = cl_get_model(dModel);
+            DongleModel = System.Text.Encoding.ASCII.GetString(dModel,0,20);
+            DisplayResult(dResult, DongleModel);
+        }
+
+        private void cllockokToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int dResult = cl_lock_ok();
+            DisplayResult(dResult, "");
+        }
+
+        private void clgetidToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dID = new byte[100];
+            String DongleID = "";
+            String rPass = "";   //read authentication password
+            int dResult = cl_get_id(dID, rPass);
+            DongleID = System.Text.Encoding.ASCII.GetString(dID,0,8);
+            DisplayResult(dResult, DongleID);
+        }
+
+        private void clgetbatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dBatch = new byte[100];
+            String DongleBatch = "";
+            String rPass = "";   //read authentication password
+            int dResult = cl_get_batch(dBatch, rPass);
+            DongleBatch = System.Text.Encoding.ASCII.GetString(dBatch,0,8);
+            DisplayResult(dResult, DongleBatch);
+        }
+
+        private void clsetsignToolStripMenuItem_Click(object sender, EventArgs e)
+        {            // dSign max length can be 75 char
+            String dSign = "Customer's Firm Name                                                       ";
+            String wPass = "";   //write authentication password
+            
+            int dResult = cl_set_sign(dSign, wPass);
+            DisplayResult(dResult, "");
+        }
+
+        private void clgetsignToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dSign = new byte[100];
+            String DongleSign = "";
+            String rPass = "";   //read authentication password
+            int dResult = cl_get_sign(dSign, rPass);
+            DongleSign = System.Text.Encoding.ASCII.GetString(dSign,0,75);
+            DisplayResult(dResult, DongleSign);
+        }
+
+        private void clsetosignToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // dSign max length can be 50 char
+            String dSign = "My Firm Name                                      ";
+            String wPass = "";   //write authentication password
+
+            int dResult = cl_set_osign(dSign, wPass);
+            DisplayResult(dResult, "");
+        }
+
+        private void clgetosignToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dSign = new byte[100];
+            String DongleOSign = "";
+            String rPass = "";   //read authentication password
+            int dResult = cl_get_osign(dSign, rPass);
+            DongleOSign = System.Text.Encoding.ASCII.GetString(dSign,0,50);
+            DisplayResult(dResult, DongleOSign);
+        }
+
+        private void clwriteblockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // dBlock1 & dBlock2 each of max length can be 100 char
+            String dBuf = "My Block1 Data                                                                                      ";
+            String wPass = "";   //write authentication password
+            int BlockNo = 1;
+            int StPos = 1;
+            int Count = 100;
+
+            int dResult = cl_write_block(dBuf, wPass, BlockNo, StPos, Count);
+            DisplayResult(dResult, "");
+        }
+
+        private void clreadblockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dBuf = new byte[100];
+            String DongleMemBlock1 = "";
+            String rPass = "";   //read authentication password
+            int BlockNo = 1;
+            int StPos = 1;
+            int Count = 100;
+
+            int dResult = cl_read_block(dBuf, rPass, BlockNo, StPos, Count);
+            DongleMemBlock1 = System.Text.Encoding.ASCII.GetString(dBuf, 0, 100);
+            DisplayResult(dResult, DongleMemBlock1);
+        }
+
+        private void clwriteblock2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // dBlock1 & dBlock2 each of max length can be 100 char
+            String dBuf = "My Block2 Data                                                                                      ";
+            String wPass = "";   //write authentication password
+            int BlockNo = 2;
+            int StPos = 1;
+            int Count = 100;
+
+            int dResult = cl_write_block(dBuf, wPass, BlockNo, StPos, Count);
+            DisplayResult(dResult, "");
+        }
+
+        private void clreadblock2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] dBuf = new byte[100];
+            String DongleMemBlock2 = "";
+            String rPass = "";   //read authentication password
+            int BlockNo = 2;
+            int StPos = 1;
+            int Count = 100;
+
+            int dResult = cl_read_block(dBuf, rPass, BlockNo, StPos, Count);
+            DongleMemBlock2 = System.Text.Encoding.ASCII.GetString(dBuf, 0, 100);
+            DisplayResult(dResult, DongleMemBlock2);
+        }
+
+        private void clwritewordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int dWord = 9999;
+            String wPass = "";   //write authentication password
+            int BlockNo = 1;
+            int StPos = 11;
+
+            int dResult = cl_write_word(dWord, wPass, BlockNo, StPos);
+            DisplayResult(dResult, "");
+        }
+
+        private void clreadwordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int dWord = 0;
+            String rPass = "";   //write authentication password
+            int BlockNo = 1;
+            int StPos = 11;
+
+            int dResult = cl_read_word(ref dWord, rPass, BlockNo, StPos);
+            DisplayResult(dResult, "");
+        }
+
+        private void cllogoutToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            int dResult = cl_logout();
+            DisplayResult(dResult, "");
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
